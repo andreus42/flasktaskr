982249c15a743ebeb0311d3f3c7b857d75560a0e
Andrew Metell <andrew.metell@gmail.com>
2019-10-13 08:54:22 -0400

    stashing changes

--- project/db_create.py
+++ project/db_create.py
@@ -1,28 +1,16 @@
-import sqlite3
-from _config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as conn:
 
-    # get a cursor object used to excute SQL commands
-    c = conn.cursor()
+# create the database and the db table
+db.create_all()
 
-    # create the table
-    c.execute("""
-        CREATE TABLE tasks(
-            task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            due_date TEXT NOT NULL,
-            priority INTEGER NOT NULL,
-            status INTEGER NOT NULL)
-        """)
 
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-    )
+# insert data
+db.session.add(Task("Finish this tutorial", date(2020, 2, 1), 10, 1))
+db.session.add(Task("Finish Real Python", date(2020, 5, 1), 10, 1))
 
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-    )
+
+# commit the changes
+db.session.commit()
